d432e270ebea1e4a303b0d216654296e7c09a4f6
TIRGANI BadreDdine <86983431+TIRGANI@users.noreply.github.com>
2023-03-07 16:04:15 +0000

    Create ShowHide_title.js

---
+++ ShowHide_title.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  ///////////////////////////////////////////
+  /******** State varaibles assignment ******/
+
+  const [show, setShow] = useState(true);
+
+  return (
+    <div className="container">
+      <button onClick={() => setShow((show) => !show)}> Show / Hide</button>
+      {show ? <h1>Welcome to React Challenges</h1> : null}
+    </div>
+  );
+}
